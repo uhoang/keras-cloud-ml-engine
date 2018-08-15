7db92b80f11ea9c8662b6c0ad9070f765b9a22cd
uyen <u.hoangth@gmail.com>
2018-08-15 18:59:19 -0400

    make tmp directory to save the trained model

--- gcloud.local.run.keras.sh
+++ gcloud.local.run.keras.sh
@@ -1,7 +1,7 @@
-mkdir -p ./tmp/sentiment_keras_hpt
+mkdir -p ./tmp/sentiment_keras
 gcloud ml-engine local train \
-  --module-name trainer.sentiment_keras_hpt \
+  --module-name trainer.sentiment_keras \
   --package-path ./trainer \
   -- \
   --train-file sentiment_set.pickle \
-  --job-dir ./tmp/sentiment_keras_hpt
+  --job-dir ./tmp/sentiment_keras
